e6d90fd9bbb7f2d9d7de9ea2003964727b06500e
Thatiane Azevedo Sugiyama <thaty.sugi@gmail.com>
2012-10-17 19:27:57 -0300

    PanelCadastrarUsuario

---
+++ SGCMF/src/sgcmf/view/PanelCadastrarUsuario.java
@@ -0,0 +1,76 @@
+package sgcmf.view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Thatiane
+ */
+public class PanelCadastrarUsuario extends JPanel {
+
+     public PanelCadastrarUsuario()
+    {
+        setLayout(new BorderLayout());
+        montaPainel();
+    }
+
+    private void montaPainel()
+    {
+        JPanel jpAux = new JPanel(new GridLayout(6, 1));
+        JPanel jpAux2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        JLabel jlLogin = new JLabel("Login:");
+        UtilView.alinhaLabel(jlLogin);
+        JLabel jlSenha = new JLabel("Senha:");
+        UtilView.alinhaLabel(jlSenha);
+        JLabel jlPerfil = new JLabel("Perfil:");
+        UtilView.alinhaLabel(jlPerfil);
+        JLabel jlNome = new JLabel("Nome:");
+        UtilView.alinhaLabel(jlNome);
+        JLabel jlEmail = new JLabel("E-mail:");
+        UtilView.alinhaLabel(jlEmail);
+        JLabel jlCPF = new JLabel("CPF:");
+        UtilView.alinhaLabel(jlCPF);
+
+        JTextField jtfLogin = new JTextField(10);
+        JTextField jtfSenha = new JTextField(10);
+        JTextField jtfPerfil = new JTextField(10);
+        JTextField jtfNome = new JTextField(10);
+        JTextField jtfEmail = new JTextField(10);
+        JTextField jtfCPF = new JTextField(10);
+
+        JButton jbCadastrar = new JButton("Cadastrar");
+        JButton jbPesquisar = new JButton("P");
+
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jlLogin));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jtfLogin, FlowLayout.LEFT));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jlSenha));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jtfSenha, FlowLayout.LEFT));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jlPerfil));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jtfPerfil, FlowLayout.LEFT));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jlNome));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jtfNome, FlowLayout.LEFT));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jlEmail));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jtfEmail, FlowLayout.LEFT));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jlCPF));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jtfCPF, FlowLayout.LEFT));
+        jpAux2.add(jbPesquisar);
+        jpAux.add(jpAux2);
+        jpAux.setBorder(BorderFactory.createEtchedBorder());
+
+
+        this.add(jpAux, BorderLayout.CENTER);
+        this.add(UtilView.putComponentInFlowLayoutPanel(jbCadastrar), BorderLayout.SOUTH);
+    }
+
+}
