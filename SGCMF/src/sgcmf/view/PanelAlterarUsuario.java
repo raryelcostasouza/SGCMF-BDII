7fa0ad6beb700433c1cbf0cfdbba9f197a228b52
Thatiane Azevedo Sugiyama <thaty.sugi@gmail.com>
2012-10-17 19:28:27 -0300

    PanelAlterarUsuario

---
+++ SGCMF/src/sgcmf/view/PanelAlterarUsuario.java
@@ -0,0 +1,141 @@
+package sgcmf.view;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import sgcmf.view.table.JTableSGCMF;
+
+/**
+ *
+ * @author Thatiane
+ */
+public class PanelAlterarUsuario extends JPanel {
+
+
+    public PanelAlterarUsuario()
+    {
+        setLayout(new BorderLayout());
+        montaPainelPrincipal();
+    }
+
+    private void montaPainelPrincipal()
+    {
+        //JPanel jpPrincipal = new JPanel(new BorderLayout());
+        JPanel jpNorth = montaPainelNorte();
+        JScrollPane jpCenter = montaPainelCentral();
+        JPanel jpSouth = montaPainelSouth();
+
+        this.add(jpNorth,BorderLayout.NORTH);
+        this.add(jpCenter,BorderLayout.CENTER);
+        this.add(jpSouth,BorderLayout.SOUTH);
+
+    }
+
+    private JPanel montaPainelNorte()
+    {
+        JPanel jpNorth = new JPanel(new BorderLayout());
+        JPanel jpEsquerda = new JPanel();
+        JPanel jpDireita = new JPanel();
+
+        JTextField jtfPesquisar = new JTextField(15);
+
+        JRadioButton jrbNome = new JRadioButton("Nome");
+        jrbNome.setSelected(true);
+        JRadioButton jrbPerfil = new JRadioButton("Perfil");
+        JRadioButton jrbLogin = new JRadioButton("Login");
+        JRadioButton jrbEmail = new JRadioButton("Email");
+
+
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(jrbNome);
+        bg.add(jrbLogin);
+        bg.add(jrbPerfil);
+        bg.add(jrbEmail);
+
+        jpEsquerda.add(jrbNome);
+        jpEsquerda.add(jrbLogin);
+        jpEsquerda.add(jrbPerfil);
+        jpEsquerda.add(jrbEmail);
+        jpEsquerda.setBorder(BorderFactory.createTitledBorder("Pesquisar por:"));
+
+        jpDireita.add(UtilView.putComponentInFlowLayoutPanel(jtfPesquisar));
+        jpDireita.setBorder(BorderFactory.createTitledBorder("Buscar:"));
+
+        jpNorth.add(jpEsquerda, BorderLayout.WEST);
+        jpNorth.add(jpDireita, BorderLayout.EAST);
+
+        return jpNorth;
+    }
+
+    private JScrollPane montaPainelCentral()
+    {
+        String[] nomeColunas =
+        {
+            "Login", "Senha", "Perfil", "Nome", "E-mail", "CPF"
+        };
+        JTableSGCMF jt = new JTableSGCMF(null, nomeColunas);
+        JScrollPane jsp = new JScrollPane(jt, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        return jsp;
+    }
+
+    private JPanel montaPainelSouth()
+    {
+        JPanel jpPrincipal = new JPanel(new BorderLayout());
+        JPanel jpAux = new JPanel(new GridLayout(3, 2));
+        JPanel jpAux2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        JLabel jlLogin = new JLabel("Login:");
+        UtilView.alinhaLabel(jlLogin);
+        JLabel jlSenha = new JLabel("Senha:");
+        UtilView.alinhaLabel(jlSenha);
+        JLabel jlPerfil = new JLabel("Perfil:");
+        UtilView.alinhaLabel(jlPerfil);
+        JLabel jlNome = new JLabel("Nome:");
+        UtilView.alinhaLabel(jlNome);
+        JLabel jlEmail = new JLabel("E-mail:");
+        UtilView.alinhaLabel(jlEmail);
+        JLabel jlCPF = new JLabel("CPF:");
+        UtilView.alinhaLabel(jlCPF);
+
+        JTextField jtfLogin = new JTextField(10);
+        JTextField jtfSenha = new JTextField(10);
+        JTextField jtfPerfil = new JTextField(10);
+        JTextField jtfNome = new JTextField(10);
+        JTextField jtfEmail = new JTextField(10);
+        JTextField jtfCPF = new JTextField(10);
+
+
+        JButton jbPesquisar = new JButton("P");
+        JButton jbAlterar = new JButton("Alterar");
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jlLogin));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jtfLogin, FlowLayout.LEFT));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jlSenha));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jtfSenha, FlowLayout.LEFT));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jlPerfil));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jtfPerfil, FlowLayout.LEFT));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jlNome));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jtfNome, FlowLayout.LEFT));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jlEmail));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jtfEmail, FlowLayout.LEFT));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jlCPF));
+        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jtfCPF, FlowLayout.LEFT));
+        jpAux2.add(jbPesquisar);
+        jpAux.add(jpAux2);
+        jpAux.setBorder(BorderFactory.createEtchedBorder());
+
+        jpPrincipal.add(jpAux, BorderLayout.CENTER);
+        jpPrincipal.add(UtilView.putComponentInFlowLayoutPanel(jbAlterar), BorderLayout.SOUTH);
+
+        return jpPrincipal;
+    }
+}
