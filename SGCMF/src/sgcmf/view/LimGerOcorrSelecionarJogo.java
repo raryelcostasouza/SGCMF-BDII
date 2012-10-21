4e37a007b734443961cd802132403c5057cfd5a7
Raryel Costa Souza <raryel.costa@gmail.com>
2012-10-21 11:16:05 -0200

    Nova tela para selecionar Jogo
    Uso de herança com consulta Jogo em vez de fazer uma nova janela do zero

---
+++ SGCMF/src/sgcmf/view/LimGerOcorrSelecionarJogo.java
@@ -0,0 +1,42 @@
+package sgcmf.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class LimGerOcorrSelecionarJogo extends LimConsultarJogo
+{
+	private LimGerenciarOcorrenciasJogo limGerenciarOcorrenciasJogo;
+	
+	public LimGerOcorrSelecionarJogo(LimBuscarJogador limBuscarJogador)
+	{
+		super();
+		setTitle("Gerenciar Ocorrências de Jogo: Selecione um Jogo");
+		setDefaultCloseOperation(HIDE_ON_CLOSE);
+		
+		limGerenciarOcorrenciasJogo = new LimGerenciarOcorrenciasJogo(limBuscarJogador);
+		mainPanel.add(montaSouthPanel(), BorderLayout.SOUTH);
+	}
+	
+	private JPanel montaSouthPanel()
+	{
+		JPanel southPanel = new JPanel();
+		
+		JButton jbGerenciarOcorrencias = new JButton("Gerenciar Ocorrências para o Jogo Selecionado");
+		jbGerenciarOcorrencias.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				limGerenciarOcorrenciasJogo.setVisible(true);
+			}
+		});
+		
+		southPanel.add(jbGerenciarOcorrencias);
+		
+		return southPanel;
+	}
+}
