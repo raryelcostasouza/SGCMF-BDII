cabd3b9776f2eddc135a0261336dd5f11b086a0f
Raryel Costa Souza <raryel.costa@gmail.com>
2012-10-21 12:40:06 -0200

    Inicialização sistema via login

--- SGCMF/src/sgcmf/control/CtrMain.java
+++ SGCMF/src/sgcmf/control/CtrMain.java
@@ -1,17 +1,52 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package sgcmf.control;
 
-/**
- *
- * @author Raryel Costa Souza
- */
+import javax.swing.JOptionPane;
+import sgcmf.view.LimLogin;
+
 public class CtrMain
 {
+	private LimLogin limLogin;
+	private CtrAdmin ctrAdmin;
+	private CtrComiteGestor ctrComiteGestor;
+	private CtrTecnico ctrTecnico;
+	private CtrEntusiasta ctrEntusiasta;
+
 	public CtrMain()
 	{
+		ctrAdmin = new CtrAdmin();
+		ctrComiteGestor = new CtrComiteGestor();
+		ctrTecnico = new CtrTecnico();
+		ctrEntusiasta = new CtrEntusiasta();
 		
+		limLogin = new LimLogin(this);
+		limLogin.setVisible(true);
+	}
+
+	public void login(String login, char[] senha)
+	{
+		if (login.equals("admin"))
+		{
+			limLogin.setVisible(false);
+			ctrAdmin.ativaTela();
+		}
+		else if (login.equals("comiteGestor"))
+		{
+			limLogin.setVisible(false);
+			ctrComiteGestor.ativaTela();
+		}
+		else if (login.equals("tecnico"))
+		{
+			limLogin.setVisible(false);
+			ctrTecnico.ativaTela();
+		}
+		else if (login.equals("entusiasta"))
+		{
+			limLogin.setVisible(false);
+			ctrEntusiasta.ativaTela();
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(null, "Login inválido!", "Erro!", JOptionPane.ERROR_MESSAGE);
+		}
 	}
 }
--- SGCMF/src/sgcmf/control/Init.java
+++ SGCMF/src/sgcmf/control/Init.java
@@ -1,15 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package sgcmf.control;
 
 import java.awt.EventQueue;
 
-/**
- *
- * @author Raryel Costa Souza
- */
 public class Init
 {
 	public static void main(String[] args)
