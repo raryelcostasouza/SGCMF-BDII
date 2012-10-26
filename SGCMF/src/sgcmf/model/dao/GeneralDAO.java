f2dd43adea782b5a6fbdba659e7dd96693bb4904
Raryel Costa Souza <raryel.costa@gmail.com>
2012-10-26 19:15:44 -0200

    Hibernate

---
+++ SGCMF/src/sgcmf/model/dao/GeneralDAO.java
@@ -0,0 +1,57 @@
+package sgcmf.model.dao;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import org.hibernate.Session;
+import sgcmf.hibernate.SGCMFHibernateUtil;
+
+public class GeneralDAO
+{
+	Session sessao;
+
+	public GeneralDAO()
+	{
+		sessao = (Session) SGCMFHibernateUtil.getSessionFactory().openSession();
+	}
+
+	public Session getSessao()
+	{
+		return sessao;
+	}
+
+	public void setSessao(Session sessao)
+	{
+		this.sessao = sessao;
+	}
+
+	public void salvar(Object entidade)
+	{
+		sessao.save(entidade);
+	}
+
+	public Object carregar(Object entidade, Serializable id)
+	{
+		sessao.load(entidade, id);
+		return entidade;
+	}
+
+	public void apagar(Object entidade)
+	{
+		sessao.delete(entidade);
+	}
+
+	public void atualizar(Object entidade)
+	{
+		sessao.update(entidade);
+	}
+
+	public void fecharSessao()
+	{
+		sessao.close();
+	}
+
+	public ArrayList<Object> listaTodos(String tabela)
+	{
+		return (ArrayList<Object>) sessao.createQuery("from " + tabela).list();
+	}
+}
