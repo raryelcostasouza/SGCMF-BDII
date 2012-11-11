531de2616a2651308ad1910fdb0ae620681ed385
Raryel Costa Souza <raryel.costa@gmail.com>
2012-11-11 11:46:12 -0200

    Atualização tela cartão ao remover

--- SGCMF/src/sgcmf/view/comiteGestor/ocorrenciaJogo/LimGerenciarOcorrenciasJogo.java
+++ SGCMF/src/sgcmf/view/comiteGestor/ocorrenciaJogo/LimGerenciarOcorrenciasJogo.java
@@ -383,6 +383,7 @@ public class LimGerenciarOcorrenciasJogo extends JDialog
                 result = ctrComiteGestor.getCtrCartao().removerCartao(idOc);
                 
                 preencheTabelaFalta();
+                preencheTabelaCartao();
             }
         }
 
