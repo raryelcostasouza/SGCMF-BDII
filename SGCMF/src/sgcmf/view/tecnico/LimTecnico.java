b32565cafa9e70bc686310b327b01cd01ae3aa6c
Raryel Costa Souza <raryel.costa@gmail.com>
2012-11-29 00:21:59 -0200

    LimTecnico
    Adição permissão acesso tela consultar seleção e consultar jogos

--- SGCMF/src/sgcmf/view/tecnico/LimTecnico.java
+++ SGCMF/src/sgcmf/view/tecnico/LimTecnico.java
@@ -5,6 +5,8 @@
 package sgcmf.view.tecnico;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -29,13 +31,15 @@ public class LimTecnico extends JFrame
     private LimGerenciarJogador limGerenciarJogador;
     private CtrTecnico ctrTecnico;
     private CtrRelatorio ctrRelatorio;
+    private final Dimension buttonDimension = new Dimension(180, 180);
+    
     public LimTecnico(CtrTecnico ctrTecnico, CtrJogo ctrJogo)
     {
         this.ctrTecnico = ctrTecnico;
         ctrRelatorio = ctrTecnico.getCtrMain().getCtrRelatorio();
         setIconImage(SGCMFIcons.LOGO.getImage());
         limGerenciarJogador = new LimGerenciarJogador(ctrTecnico);
-
+        
         setTitle("Usuário Técnico da Seleção");
         add(montaPainel());
         pack();
@@ -43,8 +47,8 @@ public class LimTecnico extends JFrame
         setVisible(false);
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         setResizable(false);
-
-
+        
+        
         addWindowListener(new WindowAdapter()
         {
             @Override
@@ -54,16 +58,38 @@ public class LimTecnico extends JFrame
             }
         });
     }
-
+    
     private JPanel montaPainel()
     {
         JPanel jpPrincipal = new JPanel(new BorderLayout());
-        JPanel jpAux = new JPanel(new GridLayout(1, 3));
-
+        
+        jpPrincipal.add(montaNorthPanel(), BorderLayout.NORTH);
+        jpPrincipal.add(montaCenterPanel(), BorderLayout.CENTER);
+        
+        JButton jbLogout = new JButton("Logout", SGCMFIcons.LOGOUT);
+        jbLogout.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                acaoLogout();
+            }
+        });
+        
+        jpPrincipal.add(UtilView.putComponentInFlowLayoutPanel(jbLogout), BorderLayout.SOUTH);
+        
+        return jpPrincipal;
+    }
+    
+    private JPanel montaNorthPanel()
+    {
+        JPanel gridPanel = new JPanel(new GridLayout(1,3));
+        
         JButton jbGerenciarJogadores = new JButton("Gerenciar Jogadores", SGCMFIcons.JOGADOR);
         jbGerenciarJogadores.setVerticalTextPosition(JButton.BOTTOM);
         jbGerenciarJogadores.setHorizontalTextPosition(JButton.CENTER);
-
+        jbGerenciarJogadores.setPreferredSize(buttonDimension);
+        
         jbGerenciarJogadores.addActionListener(new ActionListener()
         {
             @Override
@@ -72,12 +98,54 @@ public class LimTecnico extends JFrame
                 limGerenciarJogador.setVisible(true);
             }
         });
+        
+        JButton jbConsultarSelecao = new JButton("Consultar Seleções", SGCMFIcons.SELECAO);
+        jbConsultarSelecao.setVerticalTextPosition(JButton.BOTTOM);
+        jbConsultarSelecao.setHorizontalTextPosition(JButton.CENTER);
+        jbConsultarSelecao.setPreferredSize(buttonDimension);
+        
+        jbConsultarSelecao.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                ctrTecnico.getCtrMain().getCtrSelecao().ativaLimConsultaSelecao();
+            }
+        });
+        
+        
+        JButton jbConsultarJogos = new JButton("Consultar Jogos", SGCMFIcons.JOGO);
+        jbConsultarJogos.setVerticalTextPosition(JButton.BOTTOM);
+        jbConsultarJogos.setHorizontalTextPosition(JButton.CENTER);
+        jbConsultarJogos.setPreferredSize(buttonDimension);
+        
+        jbConsultarJogos.addActionListener(new ActionListener() {
 
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                ctrTecnico.getCtrMain().getCtrJogo().ativaLimConsultarJogo();
+            }
+        });
+        
+        gridPanel.add(UtilView.putComponentInFlowLayoutPanel(jbGerenciarJogadores));
+        gridPanel.add(UtilView.putComponentInFlowLayoutPanel(jbConsultarSelecao));
+        gridPanel.add(UtilView.putComponentInFlowLayoutPanel(jbConsultarJogos));
+        
+        return gridPanel;
+    }
+    
+    private JPanel montaCenterPanel()
+    {
+        JPanel centerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        
         JButton jbRelatorios = new JButton("Relatórios", SGCMFIcons.RELATORIO);
         jbRelatorios.setVerticalTextPosition(JButton.BOTTOM);
         jbRelatorios.setHorizontalTextPosition(JButton.CENTER);
-        jbRelatorios.addActionListener(new ActionListener() {
-
+        jbRelatorios.setPreferredSize(buttonDimension);
+        
+        jbRelatorios.addActionListener(new ActionListener()
+        {
             @Override
             public void actionPerformed(ActionEvent e)
             {
@@ -87,43 +155,29 @@ public class LimTecnico extends JFrame
         JButton jbTabelaCampeonato = new JButton("Tabela do Campeonato", SGCMFIcons.TABELA);
         jbTabelaCampeonato.setVerticalTextPosition(JButton.BOTTOM);
         jbTabelaCampeonato.setHorizontalTextPosition(JButton.CENTER);
-        jbTabelaCampeonato.addActionListener(new ActionListener() {
-
+        jbTabelaCampeonato.setPreferredSize(buttonDimension);
+        
+        jbTabelaCampeonato.addActionListener(new ActionListener()
+        {
             @Override
             public void actionPerformed(ActionEvent e)
             {
                 ctrTecnico.getCtrMain().getCtrTabelaCampeonato().ativaTela();
             }
         });
-                
-
-        JButton jbLogout = new JButton("Logout", SGCMFIcons.LOGOUT);
-        jbLogout.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                acaoLogout();
-            }
-        });
-
-        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jbGerenciarJogadores));
-        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jbRelatorios));
         
-        jpAux.add(UtilView.putComponentInFlowLayoutPanel(jbTabelaCampeonato));
-
-        jpPrincipal.add(jpAux, BorderLayout.CENTER);
-        jpPrincipal.add(UtilView.putComponentInFlowLayoutPanel(jbLogout), BorderLayout.SOUTH);
-
-        return jpPrincipal;
+        centerPanel.add(jbRelatorios);
+        centerPanel.add(jbTabelaCampeonato);
+        
+        return centerPanel;
     }
-
+    
     private void acaoLogout()
     {
         int op;
-
+        
         op = JOptionPane.showConfirmDialog(this, "Tem certeza que deseja fazer logout do sistema?", "Confirmação de Logout", JOptionPane.YES_NO_OPTION);
-
+        
         if (op == JOptionPane.OK_OPTION)
         {
             setVisible(false);
