eeca73c75cd0c1168d881fed603454ff2c771335
Raryel Costa Souza <raryel.costa@gmail.com>
2012-12-02 12:32:12 -0200

    Ajuste permissões app

--- SGCMF/src/sgcmf/sql/user.sql
+++ SGCMF/src/sgcmf/sql/user.sql
@@ -1,18 +1,16 @@
 create user javaAppSGCMF password 'java';
 
 grant select, insert, delete, update on table Usuario to javaAppSGCMF;
-grant select, insert, delete, update on table Selecao to javaAppSGCMF;
-grant select, insert, delete, update on table Jogo to javaAppSGCMF;
+grant select on table Selecao to javaAppSGCMF;
+grant select on table Jogo to javaAppSGCMF;
 grant select, insert, delete, update on table Jogador to javaAppSGCMF;
-grant select, insert, delete, update on table Ocorrencia to javaAppSGCMF;
-grant select, insert, delete, update on table Gol to javaAppSGCMF;
-grant select, insert, delete, update on table Cartao to javaAppSGCMF;
-grant select, insert, delete, update on table Falta to javaAppSGCMF;
-grant select, insert, delete, update on table Substituicao to javaAppSGCMF;
+grant select, insert, delete on table Ocorrencia to javaAppSGCMF;
+grant select, insert, delete on table Gol to javaAppSGCMF;
+grant select, insert, delete on table Cartao to javaAppSGCMF;
+grant select, insert, delete on table Falta to javaAppSGCMF;javaAppSGCMF
+grant select, insert, delete on table Substituicao to javaAppSGCMF;
 
 grant select, update on sequence ocorrencia_id_seq to javaAppSGCMF;
 grant select, update on sequence jogador_id_seq to javaAppSGCMF;
-grant select, update on sequence jogo_id_seq to javaAppSGCMF;
-grant select, update on sequence selecao_id_seq to javaAppSGCMF;
 grant select, update on sequence usuario_id_seq to javaAppSGCMF;
 grant select, update on sequence cartao_id_seq to javaAppSGCMF;
